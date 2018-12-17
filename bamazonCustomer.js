ebfb7bdd5959a3a153857f0495cab9cb3590cf39
May Z <maydeyn@gmail.com>
2018-12-16 21:15:01 -0600

    updated pseudo code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,18 @@
+// Pseudo Code
+// 1. Set up modules, mySQL connections. Install all NPM packages required for this app.
+// 2. Set up table display using cli-table module
+// 3. Display all products info as a table as soon as user run this app
+// 4. After the data being displayed, ask first for user input to pick a product that they would like to purchase
+// 5. If the user entered a valid ID, proceed to step 6
+//    - If you user enter invalid ID, console log error and restart the app
+// 6. Confirm the users' item ID, then use another user prompt asking for the quantity of product they would like to purchase
+// 7. Compare the user input quantity with the inventory quantity, if the demand can be fulfilled, proceed to step 8
+//    - If there's insufficient stock for the user, then console log "Insufficient inventory!" and restart the app.
+// 8. Tell the user the order has been placed: item ordered + quantity + total price.
+// 9. Update the MySQL database:
+//    a) matching the item ID first
+//    b) substract the user quantity from stock quantity
+
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 var Table = require("cli-table");
@@ -21,9 +36,7 @@ var table = new Table({
   colWidths: [5, 45, 30, 8]
 });
 
-// generate table for all products
-// acquire user first input: product ID
-function userStart() {
+function dataDisplay() {
   var query = "SELECT * FROM products;";
   connection.query(query, function(err, res) {
     for (var i = 0; i < res.length; i++) {
@@ -34,8 +47,14 @@ function userStart() {
         res[i].price
       ]);
     }
+    var res = res;
     console.log(table.toString());
+    userStart();
   });
+}
+// generate table for all products
+// acquire user first input: product ID
+function userStart(res) {
   inquirer
     .prompt([
       {
@@ -55,8 +74,9 @@ function userStart() {
     .then(function(user) {
       var userItemID = parseInt(user.purchase);
       var stockItemID = res[i].item_id;
-      for (var i = 0; i < res.length; i++) {
-        if (stockItemID === userItemID) {
+
+      if (stockItemID === userItemID) {
+        for (var i = 0; i < res.length; i++) {
           console.log(
             "There are " +
               res[i].stock_quantity +
@@ -64,7 +84,9 @@ function userStart() {
               res[i].product_name +
               "left in stock!"
           );
-          inquirer.prompt([
+        }
+        inquirer
+          .prompt([
             {
               name: "quantity",
               type: "input",
@@ -78,24 +100,25 @@ function userStart() {
                 return false;
               }
             }
-          ]);
-          var userQuantity = parseInt(user.quantity);
-          var stock = res[i].stock_quantity;
-          if (userQuantity > stock) {
-            console.log("Insufficient quantity in stock! Sorry!");
-            restart();
-          } else {
-            connection.query("UPDATE Products SET ? WHERE ?", [
-              { stock_quantity: stock - userQuantity },
-              { item_id: userItemID }
-            ]),
-              console.log("Your order has been placed!");
-            restart();
-          }
-        } else {
-          console.log("Sorry! We cannot find the item you entered.");
-          restart();
-        }
+          ])
+          .then(function(user) {
+            var userQuantity = parseInt(user.quantity);
+            var stock = res[i].stock_quantity;
+            if (userQuantity > stock) {
+              console.log("Insufficient quantity in stock! Sorry!");
+              restart();
+            } else {
+              connection.query("UPDATE Products SET ? WHERE ?", [
+                { stock_quantity: stock - userQuantity },
+                { item_id: userItemID }
+              ]),
+                console.log("Your order has been placed!");
+              restart();
+            }
+          });
+      } else {
+        console.log("Sorry! We cannot find the item you entered.");
+        restart();
       }
     });
 
@@ -122,4 +145,4 @@ function userStart() {
       });
   }
 }
-userStart();
+dataDisplay();
